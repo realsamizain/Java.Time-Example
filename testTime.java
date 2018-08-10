fa38b5199b5cadf3cf737875305e93a1340b89dd
Sami Zain <39847467+realsamizain@users.noreply.github.com>
2018-08-10 22:07:37 +0500

    Add files via upload

---
+++ testTime.java
@@ -0,0 +1,17 @@
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+public class testTime {
+public static void main(String[] args)
+{
+ZoneId zone1 = ZoneId.of("Europe/Berlin");
+ZoneId zone2 = ZoneId.of("Brazil/East");
+LocalTime now = LocalTime.now();
+LocalTime now1 = LocalTime.now(zone1);
+LocalTime now2 = LocalTime.now(zone2);
+System.out.println("Current Time : " + now);
+System.out.println("Berlin Time : " + now1);
+System.out.println("Brazil Time : " + now2);
+long minutesBetween = ChronoUnit.MINUTES.between(now2, now1);
+System.out.println("Minutes Between Berlin and Brazil : " + minutesBetween +"mins");
+}
+}
